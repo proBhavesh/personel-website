35f8f4bd7d5f8428062414f89eccdef5d1773332
Bhavesh <bhaveshjat9950@gmail.com>
2021-06-13 21:40:46 +0530

    sitmap removed

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,12 +1,5 @@
 module.exports = {
-  siteUrl: 'https://bhavesh.pro',
-  generateRobotsTxt: true,
-  // optional
-  // robotsTxtOptions: {
-  //   additionalSitemaps: [
-  //     'https://example.com/my-custom-sitemap-1.xml',
-  //     'https://example.com/my-custom-sitemap-2.xml',
-  //     'https://example.com/my-custom-sitemap-3.xml',
-  //   ],
-  // },
-};
+  siteUrl: process.env.SITE_URL || 'https://bhavesh.pro',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
