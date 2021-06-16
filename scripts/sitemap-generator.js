5eff8776287d6000080ee205d3deb9bb0edeb874
Bhavesh <bhaveshjat9950@gmail.com>
2021-06-16 12:57:11 +0530

    sitemap generator added

--- scripts/sitemap-generator.js
+++
@@ -1,8 +0,0 @@
-const sitemap = require("nextjs-sitemap-generator");
-const path = require("path");
-
-sitemap({
-	baseUrl: "bhavesh.pro",
-	pagesDirectory: path.resolve(__dirname, "../public/"),
-	targetDirectory: path.resolve(__dirname, "../public/"),
-});
